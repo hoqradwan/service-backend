6e4f9dcc78ac5edbbded08a1e855237c512c3464
SouravArefin <khansourav58@gmail.com>
2024-07-31 21:57:10 +0600

    user route done

--- src/modules/user/user.route.js
+++ src/modules/user/user.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { deleteUser, loginUser, registerUser, updateUser } from './user.controller.js';
+import { deleteUser, getUserInfo, loginUser, registerUser, updateUser } from './user.controller.js';
+import { adminMiddleware } from '../../middleware/isAdmin.js';
 
 const router = express.Router();
 
@@ -9,5 +10,6 @@ router.post('/login', loginUser);
 router.put('/update/:userId', updateUser);
 router.delete('/delete/:userId', deleteUser);
 
+router.get('/information/:id', adminMiddleware, getUserInfo);
 
 export default router;
