ae5828f472b98d267766ef0e2145b0571a6e3b05
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-08-01 20:50:06 +0600

    cron

--- src/modules/license/license.utils.js
+++ src/modules/license/license.utils.js
@@ -10,15 +10,26 @@ export const generateLicenseKey = (length = 32) => {
     .replace(/\//g, '1')
     .substring(0, length);
 };
+const formatDateWithOffset = (date) => {
+  // Convert to UTC ISO string
+  const isoString = date.toISOString(); // Example: "2024-08-01T20:20:38.366Z"
+
+  // Replace 'Z' with '+00:00' to match the desired format
+  const formattedDate = isoString.replace('Z', '+00:00');
+
+  return formattedDate;
+};
 
 // checking if the license is expired or not
 export const updateLicenseStatus = async () => {
   const now = new Date();
-  console.log(`Running updateLicenseStatus at ${now.toISOString()}`);
+  const formattedDate = formatDateWithOffset(now);
+
+  console.log(`Running updateLicenseStatus at ${formattedDate}`);
 
   try {
     const result = await LicenseModel.updateMany(
-      { expiryDate: { $lte: now }, status: { $ne: 'expired' } },
+      { expiryDate: { $lte: formattedDate }, status: { $ne: 'expired' } },
       { $set: { status: 'expired' } },
     );
     console.log('Update result:', result);
