e618619976f55a541e8c3d24112ca3cb54e56260
Thejellyfish1024 <alamin102410@gmail.com>
2024-08-08 15:34:13 +0600

    removed extra comments

--- src/middleware/isAdmin.js
+++ src/middleware/isAdmin.js
@@ -1,38 +1,3 @@
-// import { UserModel } from "../modules/user/user.model.js";
-
-
-// export const adminMiddleware = async (req, res, next) => {
-//     try {
-//         const userId = req.params.id;
-
-//         const user = await UserModel.findById(userId);
-
-//         if (!user) {
-//             return res.status(404).json({
-//                 isOk: false,
-//                 message: "User not found."
-//             });
-//         }
-
-//         if (user.role === 'admin') {
-//             // User is an admin
-//             req.isAdmin = true;
-//         } else {
-//             // User is not an admin
-//             req.isAdmin = false;
-//         }
-
-//         req.userId = userId;
-//         next();
-//     } catch (error) {
-//         console.error("Error in admin middleware:", error);
-//         return res.status(500).json({
-//             isOk: false,
-//             message: "Internal server error"
-//         });
-//     }
-// };..,
-
 import jwt from 'jsonwebtoken';
 
 export const adminMiddleware = (role) => {
