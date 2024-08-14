b20016f413154fc0e5dea53781f73e3c1bf683b4
SouravArefin <khansourav58@gmail.com>
2024-08-14 18:03:14 +0600

    cron add

--- app.js
+++ app.js
@@ -34,18 +34,7 @@ cron.schedule('0 0 * * *', () => {
     console.error('Error updating license status:', err),
   );
 });
-// cron.schedule('* * * * *', () => {
-//   console.log('Running license status update...');
-//   updateLicenseStatus().catch((err) => {
-//     console.error('Error updating license status:', err);..
-//   });
-// });
-// cron.schedule('* * * * *', () => {
-//   console.log('Running license status update...');
-//   updateLicenseStatus().catch((err) => {
-//     console.error('Error updating license status:', err);
-//   });
-// });
+
 app.use('*', notFoundRoute);
 app.use(globalErrorHandler);
 app.listen(PORT, () => {
