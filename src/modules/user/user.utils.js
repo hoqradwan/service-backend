fbc7090a41137d62e51bfd8dfd395190fc38700b
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-09-10 19:34:43 +0600

    ip track

--- src/modules/user/user.utils.js
+++ src/modules/user/user.utils.js
@@ -7,7 +7,7 @@ export const hashPassword = (password) => {
 };
 
 export const generateToken = (payload) => {
-  const expiresIn = '4h'; // 7 days expire
+  const expiresIn = '2h'; // 7 days expire
   return jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn });
 };
 
